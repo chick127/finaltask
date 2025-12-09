54ce19d30c973ab3965efe9c5203cd937366417c
chick127 <coamos1110@gmail.com>
2025-12-10 06:24:33 +0900

    9st

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,10 @@
-import { auth } from '@/auth'
+import auth from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
 
 export default auth((req) => {
-  const session = req.auth // 여기서 세션 얻음
-  const { pathname } = req.nextUrl
-
-  const isLoggedIn = !!session?.user
-  const isProtectedPage = ['/project'].some((p) => pathname.startsWith(p))
-
-  // 로그인 안 했는데 보호 페이지 접근 → 로그인 페이지로 redirect
-  if (isProtectedPage && !isLoggedIn) {
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
-
   return NextResponse.next()
 })
 
-// middleware가 동작할 경로
 export const config = {
-  matcher: ['/project/:path*'],
+  matcher: ['/about', '/project', '/team'],
 }
